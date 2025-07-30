ab9ca653b2ce261272a0dd7889909934bbdeec7a
Zaichenko Vladyslav <zaichenkovladyslav@gmail.com>
2025-07-30 15:50:02 -0600

    ImagesService.java imports are cleaned

--- src/main/java/com/broker/price/opinion/service/ImagesService.java
+++ src/main/java/com/broker/price/opinion/service/ImagesService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
