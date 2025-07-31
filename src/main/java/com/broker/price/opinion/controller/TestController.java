2e07dcce00125e15feaa4f8505b03d40b4d2af42
Zaichenko Vladyslav <zaichenkovladyslav@gmail.com>
2025-07-31 11:42:57 -0600

    BPO Information bug fixes

--- src/main/java/com/broker/price/opinion/controller/TestController.java
+++ src/main/java/com/broker/price/opinion/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.broker.price.opinion.controller;
 
+import com.broker.price.opinion.service.BrokerPriceOpinionPDFInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
 
+    @Autowired
+    private BrokerPriceOpinionPDFInfoService service;
+
     @CrossOrigin
     @GetMapping()
     public void test() {
-
+        // service.generateBPOInformation(19, "245558635");
+        // service.generateBPOInformation(20, "245560024");
     }
 }
